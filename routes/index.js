b18b2b7b955d4cd38012df7870314b331ae822d0
Vinay <x23203595@student.ncirl.ie>
2024-04-09 02:27:31 +0100

    signin with Metamask

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Ad = require('../models/ad');
+router.use(express.static(__dirname));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
