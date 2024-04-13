7a8291d8258eaaf85afd1a96d939ed96ef1f7b7e
Vinay <x23203595@student.ncirl.ie>
2024-04-13 22:08:23 +0100

    changes to price

--- seed/adseeder.js
+++ seed/adseeder.js
@@ -8,56 +8,56 @@ var ads = [
         imagePath: '/TokenGraphAdsApp/public/images/cityscape.jpg',
         title: 'City Scape',
         description: 'City',
-        price: 0.4,
+        price: 0.004,
         order: 1
     },
     {
         imagePath: '/TokenGraphAdsApp/public/images/night.jpg',
         title: 'Night',
         description: 'Night Time',
-        price: 0.2,
+        price: 0.002,
         order: 2
     },
     {
         imagePath: '/TokenGraphAdsApp/public/images/muddybites.jpg',
         title: 'Muddy Bites',
         description: 'Muddy Bites Snack',
-        price: 0.1,
+        price: 0.001,
         order: 3
     },
     {
         imagePath: '/TokenGraphAdsApp/public/images/bus.jpg',
         title: 'Bus Rush',
         description: 'Buses Rush',
-        price: 0.3,
+        price: 0.003,
         order: 4
     },
     {
         imagePath: '/TokenGraphAdsApp/public/images/burger.jpg',
         title: 'Burger',
         description: 'Sizzling!',
-        price: 0.2,
+        price: 0.002,
         order: 5 
     },
     {
         imagePath: '/TokenGraphAdsApp/public/images/london.jpg',
         title: 'London',
         description: 'London City',
-        price: 0.1,
+        price: 0.001,
         order: 6
     },
     {
         imagePath: '/TokenGraphAdsApp/public/images/car.jpg',
         title: 'Snow Car',
         description: 'Car',
-        price: 0.2,
+        price: 0.002,
         order: 7
     },
     {
         imagePath: '/TokenGraphAdsApp/public/images/coffee.jpg',
         title: 'Coffee',
         description: 'Fancy some Coffee?',
-        price: 0.1,
+        price: 0.001,
         order: 8
     },
 ];
